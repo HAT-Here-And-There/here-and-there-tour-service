8b0d2b5238724da1c757dffeae2b0fd16f5bf32a
10000001a <pgdn06288888@gmail.com>
2024-08-22 01:12:38 +0900

    feat: add count to field

--- src/main/java/com/hat/hereandthere/tourservice/domains/place/PlaceController.java
+++ src/main/java/com/hat/hereandthere/tourservice/domains/place/PlaceController.java
@@ -6,6 +6,7 @@ import com.hat.hereandthere.tourservice.domains.place.model.dto.GetPlacesPageDto
 import com.hat.hereandthere.tourservice.domains.place.model.response.GetPlaceDetailRes;
 import com.hat.hereandthere.tourservice.domains.place.model.response.GetPlacesRes;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/places")
 @RequiredArgsConstructor
@@ -23,23 +25,30 @@ public class PlaceController {
 
     @GetMapping
     public ResponseEntity<GetPlacesRes> getPlaces(
-        @RequestParam(value = "majorRegionId", required = false) Long majorRegionid,
-        @RequestParam(value = "areaId", required = false) String areaId,
-        @RequestParam(value = "sigunguId", required = false) String sigunguId,
-        Pageable pageable
+            @RequestParam(value = "majorRegionId", required = false) Long majorRegionid,
+            @RequestParam(value = "areaId", required = false) String areaId,
+            @RequestParam(value = "sigunguId", required = false) String sigunguId,
+            Pageable pageable
     ) {
-        // TODO: 채팅개수 많은 순서대로 가져오기
         GetPlacesPageDto dto = null;
-        if (majorRegionid != null && areaId == null && sigunguId == null) {
-            dto = placeService.getPlacesPageDtoByMajorRegionId(majorRegionid, pageable);
+
+        // 모든 여행지
+        if (majorRegionid == null && areaId == null && sigunguId == null) {
+            dto = placeService.getPlaces(pageable);
         }
 
+        // 8권역 필터
+        if (majorRegionid != null && areaId == null && sigunguId == null) {
+            dto = placeService.getPlacesFilteredByMajorRegion(majorRegionid, pageable);
+        }
+
+        // 시군구 필터
         if (majorRegionid == null && areaId != null && sigunguId != null) {
-            dto = placeService.getPlacesPageDtoBySigungu(areaId, sigunguId, pageable);
+            dto = placeService.getPlacesFilteredBySigungu(areaId, sigunguId, pageable);
         }
 
         if (dto == null) {
-            dto = placeService.getPlacesPage(pageable);
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
 
         GetPlacesRes res = GetPlacesRes.from(dto);
